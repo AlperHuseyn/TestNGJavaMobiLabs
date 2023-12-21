e92ba0aedd83134752a6ec6f4404fd8ea4673a7f
Alper Huseyin Dogan <alperdogan@hotmail.com>
2023-12-21 06:16:57 +0300

    Create loginApp method

--- Android/src/test/java/com/Fundamentals.java
+++ Android/src/test/java/com/Fundamentals.java
@@ -1,11 +1,35 @@
 package com;
 
+import java.time.Duration;
+
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.Test;
 
+import io.appium.java_client.AppiumBy;
+
 public class Fundamentals extends BaseTest{
+    ConfigReader config = new ConfigReader();
+
+    /**
+     * Test method to log into the application.
+     * Configuration parameters are retrieved from a properties file.
+     */
     @Test
     public void loginApp(){
-        // ...
-    }
+        // Ensure elements are visible and clickable before interaction.
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(15));
 
+        // Input email
+        wait.until(ExpectedConditions.visibilityOfElementLocated(AppiumBy.id(config.getProperty("emailLocator"))))
+            .sendKeys(config.getProperty("EMAIL"));
+
+        // Input password
+        wait.until(ExpectedConditions.visibilityOfElementLocated(AppiumBy.id(config.getProperty("passwordLocator"))))
+            .sendKeys(config.getProperty("PASSWORD"));
+        
+        // Submit login form by clicking login button
+        wait.until(ExpectedConditions.elementToBeClickable(AppiumBy.id(config.getProperty("loginButtonLocator"))))
+            .click();
+    }
 }
