a4ece4731c109e2c4796eba2af87b7c6834a53e6
Alper Huseyin Dogan <alperdogan@hotmail.com>
2023-12-23 13:56:49 +0300

    Update isDeviceAdded helper method

--- Android/src/test/java/com/Fundamentals.java
+++ Android/src/test/java/com/Fundamentals.java
@@ -46,7 +46,7 @@ public class Fundamentals extends BaseTest{
      */
     private boolean isDeviceAdded(){
         try{
-            driver.findElement(AppiumBy.id(""));
+            driver.findElement(AppiumBy.id(config.getProperty("applianceNameLocator")));
             return true;  // Element is found, return true
         } catch (NoSuchElementException err) {
             return false;  // Element not found, return false
